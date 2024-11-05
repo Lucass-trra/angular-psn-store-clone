cb5fb5457b005c187e9001522345ed8b7b8104a2
Lucas_Terra <lucasterracontato@gmail.com>
2024-11-05 12:06:44 -0300

    add game data from json file

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  data: any[] = [];
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
+    this.http.get<any>('assets/data.json').subscribe(response => {
+      this.data = response;
+    })
   }
 
 }
